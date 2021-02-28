98709b4a81c1966b5210dddde26c17a65f071fb2
Saiz <daniel.saiz@gmx.at>
2021-02-28 14:08:06 +0100

    updated routes

--- marvel-web-app/src/app/app-routing.module.ts
+++ marvel-web-app/src/app/app-routing.module.ts
@@ -3,29 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ComicDetailComponent } from './components/comic-detail/comic-detail.component';
 import { FavoritesComponent } from './components/favorites/favorites.component';
+import { ComicsComponent } from './comics/comics.component';
+import { ComicListComponent } from './components/comic-list/comic-list.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent, children: [
+  { path: 'comics', children: [
+    { path: '', component: ComicsComponent },
     { path: 'comic/:id', component: ComicDetailComponent }
   ]},
+  { path: 'home', component: HomeComponent },
   { path: 'favorites', component: FavoritesComponent },
   { path: '**', redirectTo: '/home' }
 ];
-
-/*
-{ path: '', redirectTo: '/events', pathMatch: 'full' },
-  { path: 'terms', component: TermsComponent, resolve:[RouteResolverService] },
-  { path: 'events', component: CountrySelectionComponent, resolve:[RouteResolverService] },
-  { path: ':lang', children: [
-    { path: '', redirectTo: 'events', pathMatch: 'full' },
-    { path: 'events', component: EventListComponent },
-    { path: 'event/:id', component: EventDetailComponent },
-    { path: 'terms', component: TermsComponent },
-  ], resolve:[RouteResolverService], runGuardsAndResolvers: "always" },
-  // runGuardsAndResolvers is needed for rerunning resolver, when user enters in detail-view and goes back to event overview
-  { path: '**', redirectTo: '/events' }
-*/
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
