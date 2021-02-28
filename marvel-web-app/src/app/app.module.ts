27a8623dd70c1101aae8c95f9de88606bb30964a
Saiz <daniel.saiz@gmx.at>
2021-02-28 14:03:24 +0100

    added loader component

--- marvel-web-app/src/app/app.module.ts
+++ marvel-web-app/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +13,9 @@ import { HomeComponent } from './components/home/home.component';
 import { ComicListComponent } from './components/comic-list/comic-list.component';
 import { ComicDetailComponent } from './components/comic-detail/comic-detail.component';
 import { FavoritesComponent } from './components/favorites/favorites.component';
+import { GlobalConstants } from './shared/constants.global';
+import { LoaderComponent } from './shared/loader/loader.component';
+import { ComicsComponent } from './comics/comics.component';
 
 @NgModule({
   declarations: [
@@ -21,14 +25,17 @@ import { FavoritesComponent } from './components/favorites/favorites.component';
     HomeComponent,
     ComicListComponent,
     ComicDetailComponent,
-    FavoritesComponent
+    FavoritesComponent,
+    LoaderComponent,
+    ComicsComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [GlobalConstants],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
