27853f937f376111c7e957be42e4de4f0ded5f63
Saiz <daniel.saiz@gmx.at>
2021-05-24 12:34:27 +0200

    outsourced cypress commands

--- cypress-testsuite/cypress/integration/bak2021 testsuite/szenario6.spec.ts
+++ cypress-testsuite/cypress/integration/bak2021 testsuite/szenario6.spec.ts
@@ -1,18 +1,4 @@
 
-let LOCAL_STORAGE_MEMORY = {};
-
-Cypress.Commands.add("saveLocalStorage", () => {
-  Object.keys(localStorage).forEach(key => {
-    LOCAL_STORAGE_MEMORY[key] = localStorage[key];
-  });
-});
-
-Cypress.Commands.add("restoreLocalStorage", () => {
-  Object.keys(LOCAL_STORAGE_MEMORY).forEach(key => {
-    localStorage.setItem(key, LOCAL_STORAGE_MEMORY[key]);
-  });
-});
-
 describe('Scenario 6 - Protractor', () => {
 
   before(() => {
--- cypress-testsuite/cypress/integration/bak2021 testsuite/szenario7.spec.ts
+++ cypress-testsuite/cypress/integration/bak2021 testsuite/szenario7.spec.ts
@@ -5,32 +5,44 @@ describe('Scenario 7 - Protractor', () => {
       const userData = {
         email:"kwm@e2e.at",
         id:"nuBIxcCj7Uc6Qm028LNKAUvUXgj1",
-        _token:"eyJhbGciOiJSUzI1NiIsImtpZCI6ImNjM2Y0ZThiMmYxZDAyZjBlYTRiMWJkZGU1NWFkZDhiMDhiYzUzODYiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vYmFrMjAyMS1tYXJ2ZWwtY29taWNzIiwiYXVkIjoiYmFrMjAyMS1tYXJ2ZWwtY29taWNzIiwiYXV0aF90aW1lIjoxNjIwNDg0MjY3LCJ1c2VyX2lkIjoibnVCSXhjQ2o3VWM2UW0wMjhMTktBVXZVWGdqMSIsInN1YiI6Im51Qkl4Y0NqN1VjNlFtMDI4TE5LQVV2VVhnajEiLCJpYXQiOjE2MjA0ODQyNjcsImV4cCI6MTYyMDQ4Nzg2NywiZW1haWwiOiJrd21AZTJlLmF0IiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7ImVtYWlsIjpbImt3bUBlMmUuYXQiXX0sInNpZ25faW5fcHJvdmlkZXIiOiJwYXNzd29yZCJ9fQ.Jqjui1rNa4uKKhSym921HZU_NYcJ0nvTMWQLBAta3UDjBfEGHG7-mPAGz1MFhm25sJM28UCq-ArJMXcvXOvNLqI2M8JVh6U9vmT8U42Hka7_FyvdtOyhIvI_nipB-rsO8EVuDWJEFqgefp9ZE6kwKEJ-47wmN8xXkoAyKLKBiHxddCpg9KXgRJcci_Z94FOTL0R64Wz4UkLRjUWYuoIHeNsCEJuiARcsZhIWV28Zy1uGrXjXk6DwB1aAReOOq9mKlxk0HTs9Efp9z5jB4MB7HjFLdHrIy4rd-3-ouuOgMWjmiPfBpeQpImjVak1lTfzR64wM2O36BWoG9fDVPkUKyQ",
-        _tokenExpirationDate:"2099-12-01T15:31:07.941Z"
+        _token: "eyJhbGciOiJSUzI1NiIsImtpZCI6IjNkOWNmYWE4OGVmMDViNDI0YmU2MjA1ZjQ2YjE4OGQ3MzI1N2JjNDIiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vYmFrMjAyMS1tYXJ2ZWwtY29taWNzIiwiYXVkIjoiYmFrMjAyMS1tYXJ2ZWwtY29taWNzIiwiYXV0aF90aW1lIjoxNjIxODUxMzg5LCJ1c2VyX2lkIjoibnVCSXhjQ2o3VWM2UW0wMjhMTktBVXZVWGdqMSIsInN1YiI6Im51Qkl4Y0NqN1VjNlFtMDI4TE5LQVV2VVhnajEiLCJpYXQiOjE2MjE4NTEzODksImV4cCI6MTYyMTg1NDk4OSwiZW1haWwiOiJrd21AZTJlLmF0IiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7ImVtYWlsIjpbImt3bUBlMmUuYXQiXX0sInNpZ25faW5fcHJvdmlkZXIiOiJwYXNzd29yZCJ9fQ.ZeYD7dA8u0TFKmGyTojz9a8b-yvz7kBSJ7WPSUjwNuVTciRpVmawDDvw-O3KeAf0C_iqpc8hbY_h5tGxwjEInnJWjlwTIKAsgs06MzPNmLbDvv8g5kkgNkQOPZ7V8655Oh9DFur44iMey3bByZ_3cmdLHhkaiFZm9AKjO2iDIUXKmPvpb84QMIM0eetE9tUxoCPLPyqeVEHMu1LvpIBQwIAtR3kNxKKNv_Cv7AxVB81I9vGcORouoSFghu6nua4CP_GlaCx8eKMtIS2QFf_B9WKsOEoEQXRN6ErfNFCtpkaeyoFo_3LAQ7UTUvpX8QA15Pc36mvKd7AfJ4MNOfwJWA",
+        _tokenExpirationDate:"2030-05-25T11:16:29.560Z"
       }
       window.localStorage.setItem('userData', JSON.stringify(userData))
-      console.log(window.localStorage.getItem('userData'))
+      cy.saveLocalStorage();
       cy.reload()
     })
   }) 
 
+  beforeEach(() => {
+    cy.restoreLocalStorage();
+    cy.window().then(window => console.log("=== ", window.localStorage));
+  });
+
+  afterEach(() => {
+    cy.saveLocalStorage();
+  });
   
   it('check if user is logged in and located in app-favorites', () => {
+    cy.window().then(window => console.log("1 === ", window.localStorage));
     cy.window().then(window => {
-      if(window.localStorage.getItem('userData') != null) 
+      if(window.localStorage.getItem('userData') != null) {
         cy.get('.nav-item a[routerlink="favorites"]').click()
         cy.get('app-favorites').should('exist')
         cy.get('.comic-item').should('not.exist')
+      }        
     })
   })
 
   it('check if user can add a comic to favorites', () => {
+    cy.window().then(window => console.log("2 === ", window.localStorage));
     cy.get('.nav-item a[routerlink="comics"]').click()
     cy.get('.comic-item').first().click()
     cy.get('.btn-fav').should('not.be.disabled').click()
   })
 
   it('check if favored comic exists in favorites', () => {
+    cy.window().then(window => console.log("3 === ", window.localStorage));
     cy.get('.nav-item a[routerlink="favorites"]').click()
     cy.get('.comic-item').should('exist')
   })
